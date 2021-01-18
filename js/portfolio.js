c60c9fe6841601d5c4e09144f74110c5ea8510e7
Emizzari <emilie@tonnessen.com>
2021-01-18 11:30:14 +0100

    Module script

---
+++ js/portfolio.js
@@ -0,0 +1,6 @@
+
+import { projectsURL } from "./components/api/url.js";
+import { fetchAPI } from "./components/api/fetch.js";
+import createPortfolioLayout from "./components/html/createPortfolioLayout.js";
+
+fetchAPI(createPortfolioLayout, projectsURL);
