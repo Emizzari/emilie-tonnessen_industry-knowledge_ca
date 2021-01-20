5955e91602f93b2d6dca3fd44c7e1e6af07166b3
Emizzari <emilie@tonnessen.com>
2021-01-20 18:09:03 +0100

    Form variables

---
+++ js/components/form/formVariables.js
@@ -0,0 +1,21 @@
+export const form = document.querySelector(".contact-form");
+export const feedback = document.querySelector(".feedback");
+
+// Name:
+export const name = document.querySelector("#name");
+export const nameError = document.querySelector(".contact-form__error--name");
+
+// Email:
+export const email = document.querySelector("#email");
+export const emailError = document.querySelector(".contact-form__error--email");
+
+// Topic:
+export const topic = document.querySelector("#topic");
+export const topicError = document.querySelector(".contact-form__error--topic");
+
+// Message:
+export const message = document.querySelector("#message");
+export const messageError = document.querySelector(".contact-form__error--message");
+
+// Submit
+export const submit = document.querySelector(".contact-form__submit");
