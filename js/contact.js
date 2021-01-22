831622db2e8fbeffe4fbf6823f86bcdaddb00c8e
Emizzari <emilie@tonnessen.com>
2021-01-22 17:16:10 +0100

    Testing out mobile responsive navigation

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,4 @@
+import mobileMenu from "./components/menu/mobileMenu.js"
 import { formValidation} from "./components/form/formValidation.js";
 import { form } from "./components/form/formVariables.js";
 
